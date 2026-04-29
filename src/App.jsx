1a29e38dd385c87856cc25431b41454e55964e08
Victor Kovalchuk <vict02r05@gmail.com>
2026-04-29 16:18:23 +0300

    working on styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <GlobalStyles />
+      <div>
+        <Heading as='h1'>App</Heading>
+        <Heading as='h2'>App</Heading>
+        <Heading as='h3'>App</Heading>
+        <Button>Cheack in</Button>
+      </div>
+    </>
   )
 }
