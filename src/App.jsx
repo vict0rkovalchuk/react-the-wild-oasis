74451c23b1aa9a2134da15d743f65920eeb59a92
Victor Kovalchuk <vict02r05@gmail.com>
2026-04-29 20:45:14 +0300

    create new reusable component; using default property for the first time

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
       <div>
+        <Row type='horizontal'>
+          <div>Elem 1</div>
+          <div>Elem 2</div>
+        </Row>
+        <Row>
+          <div>Elem 1</div>
+          <div>Elem 2</div>
+        </Row>
+
         <Heading as='h1'>App</Heading>
         <Heading as='h2'>App</Heading>
         <Heading as='h3'>App</Heading>
+        <Button variation='primary' size='small'>Cheack in</Button>
+        <Button variation='secondary' size='small'>Cheack in</Button>
+        <Button variation='danger' size='small'>Cheack in</Button>
         <Button>Cheack in</Button>
+        <Button variation='secondary' size='medium'>Cheack in</Button>
+        <Button variation='danger' size='medium'>Cheack in</Button>
+        <Button variation='primary' size='large'>Cheack in</Button>
+        <Button variation='secondary' size='large'>Cheack in</Button>
+        <Button variation='danger' size='large'>Cheack in</Button>
       </div>
     </>
   )
