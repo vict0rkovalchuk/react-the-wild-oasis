eeb371f0349e8a20eb512daaac3ad53b96997854
Victor Kovalchuk <vict02r05@gmail.com>
2026-04-30 21:08:29 +0300

    building the sidebar and main navigation

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components"
+import Logo from "./Logo";
+import MainNav from "./MainNav";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -6,12 +8,16 @@ const StyledSidebar = styled.aside`
   border-radius: 1px solid var(--color-grey-100);
 
   grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 export default function Sidebar() {
   return (
     <StyledSidebar>
-      Sidebar
+      <Logo />
+      <MainNav />
     </StyledSidebar>
   )
 }
