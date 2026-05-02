de223ba52b0a1b4ef86488a689ac95906274b9d5
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-02 20:05:26 +0300

    abstracting delete cabin function into custom hook: useDeleteCabin

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteCabin } from "../../services/apiCabins";
-import toast from "react-hot-toast";
 import { useState } from "react";
 import CreateCabinForm from "./CreateCabinForm";
+import { useDeleteCabin } from "./useDeleteCabin";
 
 const TableRow = styled.div`
   display: grid;
@@ -47,21 +45,9 @@ const Discount = styled.div`
 
 export default function CabinRow({ cabin }) {
   const [showForm, setShowForm] = useState(false);
-
+  const { isDeleting, deleteCabin } = useDeleteCabin();
+  
   const { name, maxCapacity, regularPrice, discount, image, id: cabinId } = cabin;
-
-  const queryClient = useQueryClient();
-
-  const {isLoading: isDeleting, mutate} = useMutation({
-    mutationFn: id => deleteCabin(id),
-    onSuccess: () => {
-      toast.success('Successfully deleted');
-      queryClient.invalidateQueries({
-        queryKey: ['cabin']
-      });
-    },
-    onError: error => toast.error(error.message)
-  });
 
   return (
     <>
@@ -70,7 +56,7 @@ export default function CabinRow({ cabin }) {
         <Cabin>{name}</Cabin>
         <div>Fits up to {maxCapacity} guests</div>
         <Price>{formatCurrency(regularPrice)}</Price>
-        <Discount>{formatCurrency(discount)}</Discount>
+        {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
 
         <div>
           <button
@@ -79,7 +65,7 @@ export default function CabinRow({ cabin }) {
             Edit
           </button>
           <button 
-            onClick={() => mutate(cabinId)} 
+            onClick={() => deleteCabin(cabinId)} 
             disabled={isDeleting}
           >
             Delete
