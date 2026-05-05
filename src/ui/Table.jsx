ea3b558c71606bd4e398038e244490f8a40e95c7
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-05 11:40:35 +0300

    building a reusable table

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -1,3 +1,4 @@
+import { createContext, useContext } from "react";
 import styled from "styled-components";
 
 const StyledTable = styled.div`
@@ -28,16 +29,16 @@ const StyledHeader = styled(CommonRow)`
   color: var(--color-grey-600);
 `;
 
+const StyledBody = styled.section`
+  margin: 0.4rem 0;
+`;
+
 const StyledRow = styled(CommonRow)`
   padding: 1.2rem 2.4rem;
 
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-grey-100);
   }
-`;
-
-const StyledBody = styled.section`
-  margin: 0.4rem 0;
 `;
 
 const Footer = styled.footer`
@@ -58,3 +59,48 @@ const Empty = styled.p`
   text-align: center;
   margin: 2.4rem;
 `;
+
+const TableContext = createContext();
+
+export default function Table({ columns, children }) {
+  return (
+    <TableContext.Provider value={{ columns }}>
+      <StyledTable role='table'>
+        {children}
+      </StyledTable>
+    </TableContext.Provider>
+  );
+}
+
+function Header({ children }) {
+  const { columns } = useContext(TableContext);
+
+  return (
+    <StyledHeader 
+      role="row" 
+      columns={columns} 
+      as="header"
+    >
+      {children}
+    </StyledHeader>
+  )
+}
+
+function Body({ children }) {
+  
+}
+
+function Row({ children }) {
+  const { columns } = useContext(TableContext);
+
+  return (
+    <StyledRow role="row" columns={columns}>
+      {children}
+    </StyledRow>
+  )
+}
+
+Table.Header = Header;
+Table.Body = Body;
+Table.Row = Row;
+Table.Footer = Footer;
