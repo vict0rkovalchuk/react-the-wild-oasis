28dc477712f53d81d117b042acc86c439c63a284
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-07 21:02:23 +0300

    checking out a booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -13,6 +13,7 @@ import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import Empty from "../../ui/Empty";
 import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,10 +22,11 @@ const HeadingGroup = styled.div`
 `;
 
 function BookingDetail() {
-  const navigate = useNavigate();
   const { isLoading, booking } = useBooking();
+  const { isCheckingOut, checkout } = useCheckout();
   const { status, id: bookingId } = booking;
 
+  const navigate = useNavigate();
   const moveBack = useMoveBack();
 
   const statusToTagName = {
@@ -53,6 +55,15 @@ function BookingDetail() {
         {status === 'unconfirmed' && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
+          </Button>
+        )}
+        
+        {status === 'checked-in' && (
+          <Button 
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingOut}
+          >
+            Check out
           </Button>
         )}
         
---
+++ src/features/check-in-out/useCheckout.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+
+export function useCheckout() {
+  const queryClien = useQueryClient();
+
+  const { isLoading: isCheckingOut, mutate: checkout } = useMutation({
+    mutationFn: (bookingId) => updateBooking(bookingId, {
+      status: 'checked-out'
+    }),
+    onSuccess: (data) => {
+      toast.success(`Booking #${data.id} successfully checked out`);
+      queryClien.invalidateQueries({ active: true });
+    },
+    onError: () => toast.error('There was an error while checking out')
+  });
+
+  return { isCheckingOut, checkout };
+}
