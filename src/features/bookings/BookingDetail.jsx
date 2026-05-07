31fd0d4721367e9a423775485df1ba373b5076a9
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-07 21:46:37 +0300

    deleting a booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -14,6 +14,9 @@ import Spinner from "../../ui/Spinner";
 import Empty from "../../ui/Empty";
 import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -24,6 +27,7 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const { isLoading, booking } = useBooking();
   const { isCheckingOut, checkout } = useCheckout();
+  const { isDeleting, deleteBooking } = useDeleteBooking();
   const { status, id: bookingId } = booking;
 
   const navigate = useNavigate();
@@ -70,6 +74,24 @@ function BookingDetail() {
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
+        
+        <Modal>
+          <Modal.Open opens='delete'>
+            <Button variation="danger">
+              Delete
+            </Button>
+          </Modal.Open>
+
+          <Modal.Window name='delete'>
+            <ConfirmDelete 
+              resourceName='booking' 
+              onConfirm={() => deleteBooking(bookingId, {
+                onSettled: () => navigate(-1)
+              })} 
+              disabled={isDeleting}
+            />
+          </Modal.Window>
+        </Modal>
       </ButtonGroup>
     </>
   );
