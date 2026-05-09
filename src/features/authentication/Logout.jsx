16bf119d333ec5e71a34c9fa53baa91233248a18
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-09 15:41:49 +0300

    user logout

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,17 @@
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+export default function Logout() {
+  const { logout, isLoading } = useLogout();
+
+  return (
+    <ButtonIcon 
+      onClick={logout}
+      disabled={isLoading}
+    >
+      {isLoading ? <SpinnerMini /> : <HiArrowRightOnRectangle />}
+    </ButtonIcon>
+  );
+}
--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       queryClient.setQueriesData(['user'], user);
-      navigate('/');
+      navigate('/', { replace: true });
     },
     onError: error => {
       console.log(error);
@@ -19,5 +19,5 @@ export function useLogin() {
     }
   });
 
-  return { login, isLoading }
+  return { login, isLoading };
 }
