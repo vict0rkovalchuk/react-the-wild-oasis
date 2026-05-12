ade8cf359eb224c3eb7759071ee43a1ae503e172
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-12 17:02:48 +0300

    building the app header; fix useSignup hook

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 export function useSignup() {
   const { mutate: signup, isLoading } = useMutation({
-    mutationFn: ({ email, password }) => signupApi({ email, password }),
+    mutationFn: signupApi,
     onSuccess: () => {
       toast.success("Account successfully created! Please verify the new account from the user's email address");
     },
