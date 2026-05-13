9c96fc540945b2b19b64d94517779cdc0d0f7c61
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-13 10:57:19 +0300

    computing recent bookings and staus

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +11,15 @@ const StyledDashboardLayout = styled.div`
 `;
 
 export default function DashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
+  const { isLoading: isLoadingStays, stays, confirmedStays } = useRecentStays();
+
+  if(isLoadingBookings || isLoadingStays) return <Spinner />;
+
+  console.log('bookings', bookings);
+  console.log('stays', stays);
+  console.log('confirmedStays', confirmedStays);
+
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
