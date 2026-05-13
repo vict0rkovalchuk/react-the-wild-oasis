ad2fb137beb3c673243fd4ca81259dadee96f500
Victor Kovalchuk <vict02r05@gmail.com>
2026-05-13 13:29:26 +0300

    displaying stays for current day

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,20 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { isCheckingOut, checkout } = useCheckout();
+
+  function handleCheckout() {
+    checkout(bookingId);
+  }
+
   return (
-    <Button variation="primary" size="small">
+    <Button 
+      variation="primary" 
+      size="small"
+      onClick={handleCheckout}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
